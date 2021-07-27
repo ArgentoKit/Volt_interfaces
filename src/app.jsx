a43b91b3fc3fc65827bc94add8a36f19aad735f0
Aleksandr Raiko <awisenone@gmail.com>
2021-07-27 18:33:10 +0300

    Create project

---
+++ src/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import 'react-select/dist/react-select.css';
+
+render(
+    <div>
+        Place your application here
+    </div>,
+    document.getElementById('app-root'));
